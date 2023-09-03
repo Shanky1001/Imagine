09c0c60c2c6c4d49dd1d7a93974c8da073ab52f7
shanky1001 <sr1071995@gmail.com>
2023-09-04 03:41:43 +0530

    added google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import NavBar from "@/components/NavBar/NavBar";
 import Toast from "@/components/Toast/Toast";
+import Analytics from "@/utils/Analytics";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,9 @@ export default function RootLayout({
 		<html lang="en">
 			<body className={inter.className}>
 				<NavBar />
+				<Analytics
+					GA_TRACKING_ID={process.env.ANALYTICS_TRACKING_ID as string}
+				/>
 				<main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
 					{children}
 				</main>
