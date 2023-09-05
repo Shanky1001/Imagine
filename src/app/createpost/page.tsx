f4c1b2b45f52d21e25aec9d5ae6b77f790f77552
shanky1001 <sr1071995@gmail.com>
2023-09-05 17:58:06 +0530

    replacing next Image with img

--- src/app/createpost/page.tsx
+++ src/app/createpost/page.tsx
@@ -162,10 +162,11 @@ const CreatePost = () => {
 				<div className="relative mt-5 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
 					{form.photo ? (
 						<>
-							<Image
+							<img
 								src={form.photo}
 								alt={form.prompt}
-								className="w-full h-full object-contain"
+								className="w-full h-full object-contain" 
+								loading="lazy"
 							/>
 						</>
 					) : (
